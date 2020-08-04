ea525f2440fbc3159e56821fc09719ef4e1538fa
Hamid Samimi <samimi.electronics@gmail.com>
2020-08-04 19:04:44 +0430

    added ejs, views, basic routes

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ const PORT = process.env.PORT || 3000
 // Main application
 const app = express()
 
+// Template engine
+app.set('view engine', 'ejs')
+
+// Static public folder
+app.use(express.static('public'))
+
 // Basic routes
-app.get('/', (req, res) => res.send('Application works...'))
+app.get('/', (req, res) => res.render('index'))
+app.get('/download', (req, res) => {
+  res.send('/download')
+})
+app.use((req, res) => {
+  res.status(404).send('404, page not fount...')
+})
 
 app.listen(PORT, () => console.log(`Application started on http://localhost:${PORT}`))
